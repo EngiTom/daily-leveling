c3475ac928f7871cbf80abb3fc01e025d15249f1
Tom Zhang <zhangtom2088@gmail.com>
2025-11-06 12:08:07 -0800

    small change to datetime import

--- daily_leveling.py
+++ daily_leveling.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import datetime
+from datetime import datetime
 import json
 from zoneinfo import ZoneInfo
 
